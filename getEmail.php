8c1cf78f0f2977ba1e0b678ab9881f222d71f884
nauphal TF <muhammad_nauphal@ispg.in>
2012-05-08 16:07:45 +0530

    a test comment

--- getEmail.php
+++ getEmail.php
@@ -10,5 +10,14 @@ $sql		=		"SELECT * FROM
 					 coupons LEFT JOIN gifts ON coupons.id = gifts.coupon_id
 					 WHERE code='$code' LIMIT 0,1";
 $res		=		mysql_query($sql);
-print_r(mysql_fetch_assoc($res));
+$row 		=		mysql_fetch_assoc($res);
+if($row['code'] == "") {
+	echo "<h1 style=\"color:red\">Invalid Code.</h1>";
+}
+else {
+	echo "<h1 style=\"color:blue\">Your coupon has been redeemed.</h1>";
+}
+if($row['gift']) {
+	echo "<h1 style=\"color:green\">You have Won ".$row['gift']."</h1>";
+}
 ?>
